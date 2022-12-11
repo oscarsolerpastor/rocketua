01b838b229bfcd423918ceedcf4995fc099c1045
oscarsoler <oscarsolerpastor@yahoo.es>
2022-12-11 13:20:35 +0100

    Ejercicio 5 - Seeders

--- database/migrations/2022_11_25_171136_create_cart_lines_table.php
+++ database/migrations/2022_11_25_171136_create_cart_lines_table.php
@@ -28,10 +28,10 @@ return new class extends Migration
             $table->integer('total_tax_per_unit');
             $table->timestamps();
 
-            $table->integer('cart_id')->unsigned();
-            $table->foreign('cart_id')->references('id')->on('carts');
+            // $table->integer('cart_id')->unsigned();
+            // $table->foreign('cart_id')->references('id')->on('carts');
 
-            
+
         });
     }
 
--- database/migrations/2022_12_10_224455_create_favorites_table.php
+++ database/migrations/2022_12_10_224455_create_favorites_table.php
@@ -17,8 +17,8 @@ return new class extends Migration
             $table->id();
             $table->timestamps();
 
-            $table->integer('user_id')->unsigned();
-            $table->foreign('user_id')->references('id')->on('users');
+            // $table->integer('user_id')->unsigned();
+            // $table->foreign('user_id')->references('id')->on('users');
         });
     }
 
